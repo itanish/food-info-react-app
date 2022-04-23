309ad87425160e4429b5f3e1f6653a4f8b242139
rajatbhagat <rajat.bhagat@gmail.com>
2022-04-22 20:01:50 -0400

    Integrating local storage code with login

--- src/components/EditUserProfile/index.js
+++ src/components/EditUserProfile/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { updateUser } from "../../actions/user_actions";
+import NavigationBar from "../NavigationBar";
 
 const EditUserusers = () => {
     const users = useSelector(state => state.users);
     const dispatch = useDispatch();
+    const naigate = useNavigate();
+    
     const saveusers = () => {
-        updateUser(dispatch,users);
+        console.log("Saving")
+        const out = updateUser(dispatch,users);        
+        naigate("/profile")
     };
 
     const nameOnChangeHandler = (value) => {
@@ -18,32 +23,62 @@ const EditUserusers = () => {
     }
 
     return (
-    <>
+      <>
+        <NavigationBar />
         <div>
-                <div className="row mb-2">
-                    <div className="col-1">
-                        <Link to="/tuiter/users"><span className="text-white"><i class="fa fa-solid fa-window-close"></i></span></Link>
-                    </div>
-                    <div className="col-11">
-                    <span className="text-white"> <strong className="text-white">Edit users</strong></span>
-                    <Link to="/profile"><button type="button" onClick={() => saveusers()} class="btn btn-light btn-sm float-end wd-button-save">Save</button></Link>
-                    </div>
-                    
-                </div>
+          <div className="row mb-2">
+            <div className="col-1">
+              <Link to="/tuiter/users">
+                <span className="text-white">
+                  <i class="fa fa-solid fa-window-close"></i>
+                </span>
+              </Link>
             </div>
-            <div className="wd-div">
-                <label for="name" className="text-muted">Name</label><br/>
-                <input id="name" type="text" onChange={(event) => {
-                    nameOnChangeHandler(event.target.value);
-                }} className="form-control wd-input text-white bg-transparent" defaultValue={users.name}></input>
+            <div className="col-11">
+              <span className="text-white">
+                {" "}
+                <strong className="text-white">Edit users</strong>
+              </span>
+              <button
+                type="button"
+                onClick={() => saveusers()}
+                class="btn btn-light btn-sm float-end wd-button-save"
+              >
+                Save
+              </button>
             </div>
-            <div className="wd-div">
-                <label for="bio" className="text-muted">Email</label><br/>
-                <textarea id="bio" onChange={(event) => {
-                    emailOnChangeHandler(event.target.value);
-                }}  className="form-control wd-input text-white bg-transparent" defaultValue={users.email}></textarea>
-            </div>
-    </>
+          </div>
+        </div>
+        <div className="wd-div">
+          <label for="name" className="text-muted">
+            Name
+          </label>
+          <br />
+          <input
+            id="name"
+            type="text"
+            onChange={(event) => {
+              nameOnChangeHandler(event.target.value);
+            }}
+            className="form-control wd-input text-white bg-transparent"
+            defaultValue={users.name}
+          ></input>
+        </div>
+        <div className="wd-div">
+          <label for="bio" className="text-muted">
+            Email
+          </label>
+          <br />
+          <textarea
+            id="bio"
+            onChange={(event) => {
+              emailOnChangeHandler(event.target.value);
+            }}
+            className="form-control wd-input text-white bg-transparent"
+            defaultValue={users.email}
+          ></textarea>
+        </div>
+      </>
     );
 };
 export default EditUserusers;
